c69f92646011452bd62181e8ca89472314b59d30
Matt Larson <matt.a.larson@gmail.com>
2017-04-18 21:31:45 -0500

    added to it

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ function onReady(){
       $('.title').append(response.Title);
       $( '.container' ).append( '<img src="'+ response.Poster +'">');
       $('.plot').append(response.Plot);
+      $('.director').append(response.Director);
+      $('.actors').append(response.Actors);
+      $('.released').append(response.Released);
     }
   });
 }
