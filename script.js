de2dd206b2c2c0c7db5349eb2c5fcba505511a98
Matt Larson <matt.a.larson@gmail.com>
2017-04-18 22:07:48 -0500

    final touches

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ function onReady(){
     url: 'http://www.omdbapi.com/?t=i+heart+huckabees',
     success: function(response){
       console.log(response);
-      $('.title').append(response.Title);
-      $( '.container' ).append( '<img src="'+ response.Poster +'">');
-      $('.plot').append(response.Plot);
-      $('.director').append(response.Director);
-      $('.actors').append(response.Actors);
-      $('.released').append(response.Released);
+      $( '.title' ).append( response.Title );
+      $( '.poster' ).append( '<img src="' + response.Poster + '">' );
+      $( '.plot' ).append( response.Plot );
+      $( '.director' ).append( response.Director );
+      $( '.actors' ).append( response.Actors );
+      $( '.released' ).append( response.Released );
     }
   });
 }
+// "<a href=" + response.Website + "></a>"
